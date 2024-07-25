1a310b8dcf6e2465489b2836f9483275817eb363
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-25 19:20:06 +0530

    Update ResourceNotFoundException.java

--- Syna-blog-app/src/main/java/com/rusiruchapana/Syna/blog/app/exception/ResourceNotFoundException.java
+++ Syna-blog-app/src/main/java/com/rusiruchapana/Syna/blog/app/exception/ResourceNotFoundException.java
@@ -1,8 +1,9 @@
 package com.rusiruchapana.Syna.blog.app.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
 
     private String resourceName;
