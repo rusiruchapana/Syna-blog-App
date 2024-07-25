dec6d9e6975bfc6234f36dfee58c1ad005f29daa
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-25 17:04:33 +0530

    create the springboot bootsrap project.

---
+++ Syna-blog-app/src/main/java/com/rusiruchapana/Syna/blog/app/SynaBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.rusiruchapana.Syna.blog.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynaBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynaBlogAppApplication.class, args);
+	}
+
+}
---
+++ Syna-blog-app/src/test/java/com/rusiruchapana/Syna/blog/app/SynaBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rusiruchapana.Syna.blog.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynaBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
