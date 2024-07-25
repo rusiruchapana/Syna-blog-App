598597e96aa95a5585d892b5e2e78a7175640836
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-25 21:48:59 +0530

    Create PostDTO.java

---
+++ Syna-blog-app/src/main/java/com/rusiruchapana/Syna/blog/app/dto/PostDTO.java
@@ -0,0 +1,16 @@
+package com.rusiruchapana.Syna.blog.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+}
