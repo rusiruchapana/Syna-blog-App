4ffc7e7a2a13fcc774d3ffbca181170dff6a57db
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-08-05 20:51:04 +0530

    update.

--- Syna-blog-app/src/main/java/com/rusiruchapana/Syna/blog/app/entity/Post.java
+++ Syna-blog-app/src/main/java/com/rusiruchapana/Syna/blog/app/entity/Post.java
@@ -1,9 +1,7 @@
 package com.rusiruchapana.Syna.blog.app.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
--- Syna-blog-app/src/main/java/com/rusiruchapana/Syna/blog/app/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rusiruchapana.Syna.blog.app.repository;
-
-import com.rusiruchapana.Syna.blog.app.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post,Long> {
-}
