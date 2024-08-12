a2c369c895142221700b895a441df168e33c9ea1
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-08-12 18:32:23 +0530

    create basic files that need for the "post".

---
+++ Syna-blog-app/src/main/java/com/rusiruchapana/Syna/blog/app/service/PostService.java
@@ -0,0 +1,7 @@
+package com.rusiruchapana.Syna.blog.app.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface PostService {
+}
---
+++ Syna-blog-app/src/main/java/com/rusiruchapana/Syna/blog/app/service/impl/PostServiceImpl.java
@@ -0,0 +1,6 @@
+package com.rusiruchapana.Syna.blog.app.service.impl;
+
+import com.rusiruchapana.Syna.blog.app.service.PostService;
+
+public class PostServiceImpl implements PostService {
+}
