ab1991c7982f34d39c2a7b13a1a0a9cdf942258a
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-08-16 17:08:49 +0530

    Handling exceptions globally.

---
+++ Syna-blog-app/src/main/java/com/rusiruchapana/Syna/blog/app/dto/ErrorDetails.java
@@ -0,0 +1,17 @@
+package com.rusiruchapana.Syna.blog.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetails {
+
+    private Date timestamp;
+    private String message;
+    private String url;
+}
