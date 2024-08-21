1cd443c8cf75bac74e0d94cf280d1e6ae9fec849
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-08-21 15:35:48 +0530

    Create jpa query methods in user & role repositories.

--- security-test/src/main/java/com/rusiruchapana/security/test/repository/UserRepository.java
+++ security-test/src/main/java/com/rusiruchapana/security/test/repository/UserRepository.java
@@ -3,6 +3,13 @@ package com.rusiruchapana.security.test.repository;
 import com.rusiruchapana.security.test.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User , Long> {
 
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailOrUsername(String username , String email);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
