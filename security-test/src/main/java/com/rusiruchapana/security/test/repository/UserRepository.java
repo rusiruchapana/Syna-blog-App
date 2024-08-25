7794f950df693fe1a999995233c4fd312fb36066
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-08-25 09:40:40 +0530

    create user & role repository interfaces.

---
+++ security-test/src/main/java/com/rusiruchapana/security/test/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.rusiruchapana.security.test.repository;
+
+import com.rusiruchapana.security.test.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User , Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByUsernameOrEmail(String username , String email);
+    Boolean existsByEmail(String email);
+    Boolean existsByUsername(String username);
+}
