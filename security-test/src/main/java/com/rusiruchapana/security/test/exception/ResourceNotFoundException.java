09668646835bab9557247cef337f855730100e8f
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-10-27 16:18:58 +0530

    ###Start project again.

--- security-test/src/main/java/com/rusiruchapana/security/test/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.rusiruchapana.security.test.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- security-test/src/main/java/com/rusiruchapana/security/test/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rusiruchapana.security.test.repository;
-
-import com.rusiruchapana.security.test.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post , Long> {
-}
